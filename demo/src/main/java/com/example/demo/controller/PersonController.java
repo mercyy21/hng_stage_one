3b77d9390c4e558da8ab8c9f1a051755ecac2210
mercyy21 <mawopetu21@gmail.com>
2023-09-10 21:29:03 +0100

    updates

--- demo/src/main/java/com/example/demo/controller/PersonController.java
+++ demo/src/main/java/com/example/demo/controller/PersonController.java
@@ -1,4 +1,40 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+@RequestMapping("/api")
+@RestController
 public class PersonController {
+    private static List<Person> database = new ArrayList<Person>();
+    LocalDate currentDate = LocalDate.now();
+    DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
+    String dayOfWeekString = dayOfWeek.getDisplayName(TextStyle.FULL,Locale.ENGLISH);
+    public static String getCurrentUtcTime(){
+        String utcTime;
+        Instant instant = Instant.now();
+        utcTime=instant.toString();
+        return utcTime;
+    }
+    @GetMapping
+    public Optional<Person> getSlackNameAndTrack(@RequestParam(name = "slack_name") String slackName, @RequestParam(name = "track") String track){
+        database.add(new Person("Mercy A.",dayOfWeekString,getCurrentUtcTime(),"Backend","https://github.com/mercyy21/hng_stage_one/tree/master/demo/src/main/java/com/example/demo/controller","https://github.com/mercyy21/hng_stage_one",200));
+        database.add(new Person("Akin",dayOfWeekString,getCurrentUtcTime(),"Frontend","https://github.com/mercyy21/hng_stage_one/tree/master/demo/src/main/java/com/example/demo/controller","https://github.com/mercyy21/hng_stage_one",200));
+        return database.stream()
+                .filter(person -> person.getSlackName().equals(slackName)&&person.getTrack().equals(track))
+                .findFirst();
+    }
+
+
+
+
 }
